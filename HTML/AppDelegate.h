7b0c223063c766a366386ec118063226653aacad
Yuji Shimizu <yuji.shimizu@dena.com>
2014-02-01 21:45:50 -0800

    Initial Commit

---
+++ HTML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HTML
+//
+//  Created by Yuji Shimizu on 2/1/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HTML/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HTML
+//
+//  Created by Yuji Shimizu on 2/1/14.
+//  Copyright (c) 2014 Yuji Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
