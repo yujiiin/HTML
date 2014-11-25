1a68b71b007b9666311532d3bcb205a4be5569a7
Yuji Shimizu <yuji.shimizu@dena.com>
2014-11-24 22:56:02 -0800

    initial commit

--- HTML/ViewController.m
+++ HTML/ViewController.m
@@ -17,13 +17,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+/*load <a href="dotinstall.html" data-role="button">Dot install html</a> from local
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"html"];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"dotinstall" ofType:@"html"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSURLRequest *req = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:req];
+*/
+    
+    NSURLRequest* req = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://54.215.215.248/sample/jquerymobile.html"]];
+    [webView loadRequest:req];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc{
+    webView.delegate = nil;
 }
 
 @end
